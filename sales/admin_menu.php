439d3ef73f303c70dc6fab3cb9aacac9607bb527
FierySnake <35088678+FierySnake@users.noreply.github.com>
2018-12-05 12:12:53 -0800

    Add files via upload

---
+++ sales/admin_menu.php
@@ -0,0 +1,39 @@
+<ul>
+<li>
+<a href="admin.php"> </a>
+</li>
+<li>
+<a href="#" class="submenu-toggle">
+<i class="glyphicon glyphicon-th-large"></i>
+<span>Inventory</span>
+</a>
+<ul class="nav submenu">
+<li><a href="product.php"> Inventory</a> </li>
+
+<li><a href="location.php">location</a> </li>
+<li><a href="search location.php">search location</a> </li>
+<li><a href="sales.php">Make a Sale</a> </li>
+</ul>
+</li>
+
+<li>
+<a href="#" class="submenu-toggle">
+<i class="glyphicon glyphicon-signal"></i>
+<span>Report</span>
+</a>
+<ul class="nav submenu">
+<li><a href="inventorysum.php">Inventory current value </a></li>
+</ul>
+</li>
+
+<li>
+<a href="#" class="submenu-toggle">
+<i class="glyphicon glyphicon-time"></i>
+<span>Clocking System</span>
+</a>
+<ul class="nav submenu">
+<li><a href="clockingsystem.php">Clock In And Out </a></li>
+</ul>
+</li>
+
+</ul>

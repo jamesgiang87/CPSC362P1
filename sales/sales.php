0890b91e7f607a622b3ef003dd752c3cac990b73
FierySnake <35088678+FierySnake@users.noreply.github.com>
2018-12-10 00:08:04 -0800

    Add files via upload

--- sales/sales.php
+++ sales/sales.php
@@ -22,6 +22,8 @@ Fill out form and click SUBMIT:
 <form action = "sales.php" method = "post">
 <label id="first"> Barcode:</label><br>
 <input type="text" name="BARC"><br/>
+<label id="first"> Username:</label><br>
+<input type="text" name="USERNAME"><br/>
 <label id="first"> Customer Email:</label><br>
 <input type="text" name="EMAIL"><br/>
 <button type="submit" name="submit">SUBMIT</button></form>
@@ -37,67 +39,75 @@ Fill out form and click SUBMIT:
 	if (isset ($_POST['submit']))
 	{
 		
-		//check inventory delete from inventory if its there	
+		//check inventory delete from inventory if its there
+		//im assuming we have multiple of one items but same barcode		
 		$sql = "SELECT * FROM `inventory` WHERE `BARC` = '{$_POST['BARC']}'";
 		$result = mysqli_query($con,$sql);
 		
 		if (!$result || mysqli_num_rows($result)== 0 )
 		{
+			
 			echo "Item is not inventory";
 						
 		}
 		else
 		{
-		//im assuming we have multiple of one items but same barcode
-		
-		//Create Date
-		date_default_timezone_set('America/Los_Angeles');
-		$date = date("Y-m-d H:i:s");
-		//echo 'Right Now is :', $date;
-		$row = mysqli_fetch_row($result);
-		//$result_barc =  $row[0];
-		//$result_num =  $row[1];
-		//$result_name =  $row[2];
-		//$result_type =  $row[3];
-		//$result_info =  $row[4];
-		//$result_buyprice =  $row[5];	
-		//$result_sellprice =  $row[6];
-		//$result_markprice =  $row[7];		
-		//get all the infomation from $result and put it in an array or sort;
-		
-		//delete inventory
-		$sql = "DELETE FROM `inventory` WHERE `BARC` = '{$_POST['BARC']}' LIMIT 1";
-		mysqli_query($con,$sql);
-		
-		
-		//random number generator
-		$random_sales_num = rand();
-		
-		$sql = "SELECT * FROM `sales` WHERE `SALES_NUM` = '{$random_sales_num}'";
-		$result = mysqli_query($con,$sql);
-		
-		while ((!$result || mysqli_num_rows($result) != 0 ))
-		{
-			$random_sales_num = rand();
-			$sql = "SELECT * FROM `sales` WHERE `SALES_NUM` = '{$random_sales_num}'";
+			//check is the username exists
+			$sql = "SELECT * FROM `users` WHERE `username` = '{$_POST['USERNAME']}'";
 			$result = mysqli_query($con,$sql);
-		}
+			
+				if (!$result || mysqli_num_rows($result)== 0 )
+				{
+					echo "Username does not exist in database";
+				}
+			
+				else
+				{			
+				//Create Date
+				date_default_timezone_set('America/Los_Angeles');
+				$date = date("Y-m-d H:i:s");
+				//echo 'Right Now is :', $date;
+				$sql = "SELECT * FROM `inventory` WHERE `BARC` = '{$_POST['BARC']}'";
+				$result = mysqli_query($con,$sql);
+				$row = mysqli_fetch_row($result);
+				//$result_barc =  $row[0];
+				//$result_num =  $row[1];
+				//$result_name =  $row[2];
+				//$result_type =  $row[3];
+				//$result_info =  $row[4];
+				//$result_buyprice =  $row[5];	
+				//$result_sellprice =  $row[6];
+				//$result_markprice =  $row[7];		
+				//get all the infomation from $result and put it in an array or sort;
 		
-		//add to sales 
-		$sql = "INSERT INTO sales(BARCODE,PURCHASE_DATE,CUSTOMER_EMAIL, BUYPRICE,INFO,MARKPRICE,NAME,NUM,SELLPRICE,TYPE,SALES_NUM) 
-		VALUES ('{$row[0]}','{$date}','{$_POST["EMAIL"]}','{$row[5]}','{$row[4]}','{$row[7]}','{$row[2]}','{$row[1]}','{$row[6]}','{$row[3]}','{$random_sales_num}')";
-		$result = mysqli_query($con,$sql);
+				//delete inventory
+				$sql = "DELETE FROM `inventory` WHERE `BARC` = '{$_POST['BARC']}' LIMIT 1";
+				mysqli_query($con,$sql);
+				//random number generator
+				$random_sales_num = rand();
 		
-		if(!$result)
-		{
-			echo "query failed";
+				$sql = "SELECT * FROM `sales` WHERE `SALES_NUM` = '{$random_sales_num}'";
+				$result = mysqli_query($con,$sql);
+		
+				while ((!$result || mysqli_num_rows($result) != 0 ))
+				{
+					$random_sales_num = rand();
+					$sql = "SELECT * FROM `sales` WHERE `SALES_NUM` = '{$random_sales_num}'";
+					$result = mysqli_query($con,$sql);
+				}
+				
+				//add to sales 
+				$sql = "INSERT INTO sales(BARCODE,PURCHASE_DATE,CUSTOMER_EMAIL, BUYPRICE,INFO,MARKPRICE,NAME,NUM,SELLPRICE,TYPE,SALES_NUM,USERNAME) 
+				VALUES ('{$row[0]}','{$date}','{$_POST["EMAIL"]}','{$row[5]}','{$row[4]}','{$row[7]}','{$row[2]}','{$row[1]}','{$row[6]}','{$row[3]}','{$random_sales_num}','{$_POST["USERNAME"]}')";
+				$result = mysqli_query($con,$sql);
+		
+				if(!$result){
+					echo "query failed"; }
+				else{
+					echo "Product has been added";}			
+				}
 		}
-		else
-		{
-			echo "Product has been added";
-		}
-		}
-		}
+	}
 	$con->close();
 ?>
 <h1>Sales:</h1>
@@ -107,6 +117,7 @@ Fill out form and click SUBMIT:
 
 	<tr>
 		<th>SALES NUMBER</th>
+		<th>USERNAME</th>
 		<th>BARCODE</th>
 		<th>NUMBER</th>
 		<th>NAME</th>
@@ -129,6 +140,7 @@ Fill out form and click SUBMIT:
 			while($row = mysqli_fetch_array($result) ){ 
 				echo "<tr>";
 				echo "<td>".$row['SALES_NUM']."</td>";
+				echo "<td>".$row['USERNAME']."</td>";
 				echo "<td>".$row['BARCODE']."</td>";
 				echo "<td>".$row['NUM']."</td>";
 				echo "<td>".$row['NAME']."</td>";
